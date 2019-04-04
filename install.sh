66ccd24ba4181963611330601219030c8ebe69cf
norabal <norabal.works@gmail.com>
2019-04-04 11:46:32 +0900

    Ask whether replace existing file or directory

--- install.sh
+++ install.sh
@@ -26,14 +26,14 @@ do
     [[ "$f" == ".DS_Store" ]] && continue
     [[ "$f" == ".idea" ]] && continue
 
-    ln -sv "$THIS_DIR/$f" "$HOME"/"$f"
+    ln -svi "$THIS_DIR/$f" "$HOME"/"$f"
 done
 
 echo "Back to '$CURRENT_DIR'..."
 cd "$CURRENT_DIR"
 
 echo  "Create symlink for Karabiner..."
-ln -sv "$THIS_DIR/karabiner/karabiner.json" "$HOME/.config/karabiner/karabiner.json"
+ln -svi "$THIS_DIR/karabiner/karabiner.json" "$HOME/.config/karabiner/karabiner.json"
 
 echo  "Create symlink for Prezto..."
 if [[ -d "$HOME/.zprezto/runcoms" ]]; then
