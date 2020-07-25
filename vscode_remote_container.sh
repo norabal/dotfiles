c86d083e129007c8dd3795b3d313afa255eb9bb3
norabal <norabal.works@gmail.com>
2020-07-26 00:52:42 +0900

    Apply git config command

--- vscode_remote_container.sh
+++ vscode_remote_container.sh
@@ -20,6 +20,7 @@ echo "Move to '$THIS_DIR'..."
 
 cd "$THIS_DIR" || { echo "Could not move"; exit 1; }
 ln -svi "$THIS_DIR/.gitignore_global" "$HOME/.gitignore_global"
+git config --global core.excludesfile ~/.gitignore_global
 
 cat << END
 
