9d4df8573e4093284de3767de354286821094474
norabal <norabal.works@gmail.com>
2020-07-30 12:42:54 +0900

    Remove default .gitconfig

--- vscode_remote_container.sh
+++ vscode_remote_container.sh
@@ -18,6 +18,7 @@ echo "Create symlink for Hidden files..."
 echo "Move to '$THIS_DIR'..."
 
 cd "$THIS_DIR" || { echo "Could not move"; exit 1; }
+rm "$HOME/.gitconfig"
 ln -svi "$THIS_DIR/.gitconfig" "$HOME/.gitconfig"
 
 cat << END
